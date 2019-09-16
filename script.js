87f456c3e6108b4ccba54b3b44cfd39c6c09738f
Magid Drwishah <54711969+MagidDrwishah@users.noreply.github.com>
2019-09-16 19:35:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function Fun() { 
+   document.getElementById("b2").style.display="";
+   document.getElementById("b1").style.display="none";
+}
+function fun(){
+    document.getElementById("b1").style.display="";
+    document.getElementById("b2").style.display="none";
+}
