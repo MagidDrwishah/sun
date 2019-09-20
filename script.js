ecba94fa1e7be3143ed843500bdc5f59ad665a63
Magid Drwishah <54711969+MagidDrwishah@users.noreply.github.com>
2019-09-21 01:00:28 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+function f1(){
+document.getElementById("div1").style.display="";
+document.getElementById("div2").style.display="none";
+document.getElementById("div3").style.display="none";
+document.getElementById("div4").style.display="none";
+}
+
+function f2(){
+    document.getElementById("div2").style.display="";
+    document.getElementById("div1").style.display="none";
+    document.getElementById("div3").style.display="none";
+    document.getElementById("div4").style.display="none";
+}
+
+function f3(){
+    document.getElementById("div3").style.display="";
+    document.getElementById("div1").style.display="none";
+    document.getElementById("div2").style.display="none";
+    document.getElementById("div4").style.display="none";
+}
+
+function f4(){
+    document.getElementById("div4").style.display="";
+    document.getElementById("div1").style.display="none";
+    document.getElementById("div2").style.display="none";
+    document.getElementById("div3").style.display="none";
+}
