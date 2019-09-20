3ecf656e6b09ef871c5bb93cf729e4326f387043
Magid Drwishah <54711969+MagidDrwishah@users.noreply.github.com>
2019-09-21 01:00:00 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function Fun() { 
-   document.getElementById("b2").style.display="";
-   document.getElementById("b1").style.display="none";
-}
-function fun(){
-    document.getElementById("b1").style.display="";
-    document.getElementById("b2").style.display="none";
-}
